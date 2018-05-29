9827014eef13e3bf30f0c41977655380ef52979b
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2018-05-29 12:26:23 +0300

    4.1 In-mapper combining v.1

--- mapper.py
+++ mapper.py
@@ -3,6 +3,13 @@
 import sys
 
 for line in sys.stdin:
+    dict = {}
     for word in line.strip().split(' '):
-        if word: print(word + '\t1')
- 
+        if word:
+            if word in dict:
+                dict[word] = dict[word] + 1
+            else:
+                dict[word] = 1
+
+    for k, v in dict.items():
+        print(str(k) + '\t' + str(v))
