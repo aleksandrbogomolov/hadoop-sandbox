a4f1d9f6cf1e506b521ea5c4c8b2894ebba6d6ee
Aleksandr <ksandr.ua@gmail.com>
2018-06-12 11:23:58 +0300

    Complete mapper

--- 5.2/mapper.py
+++ 5.2/mapper.py
@@ -8,6 +8,7 @@ inf = 'INF'
 for line in sys.stdin:
     (key, dist, dest) = line.strip().split('\t')
     dest = re.sub(r'[{}]', '', dest)
-    dest = [int(s) for s in dest.split(',') if s != '']
-    print(key, dist, dest)
-    
+    print('%s\t%s\t{%s}' % (key, dist, dest))
+    dist = int(dist) + 1 if dist != inf else inf
+    for s in [s for s in dest.split(',') if s != '']:
+            print('%s\t%s\t{}' % (s, str(dist)))
