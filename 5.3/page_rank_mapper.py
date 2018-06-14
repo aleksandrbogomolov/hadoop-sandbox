f03fccfdb281b6292c46864146b1147e1c7dedaf
Aleksandr <ksandr.ua@gmail.com>
2018-06-14 21:28:34 +0300

    Implemented page rank mapper

--- 5.3/page_rank_mapper.py
+++ 5.3/page_rank_mapper.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
+for line in sys.stdin:
+    (key, page_rank, dest) = line.strip().split('\t')
+    print(line.strip())
+
+    dest = re.sub(r'[{}]', '', dest)
+    dest = dest.split(',')
+    for d in dest:
+        print('%s\t%.3f\t{}' % (d, float(page_rank) / len(dest)))
