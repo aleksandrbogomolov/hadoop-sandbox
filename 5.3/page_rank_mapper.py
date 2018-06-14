1bc3409dfcbe7759ad90be67d8ae524ada704c98
Aleksandr <ksandr.ua@gmail.com>
2018-06-14 21:43:32 +0300

    Removed regex from mapper

--- 5.3/page_rank_mapper.py
+++ 5.3/page_rank_mapper.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
-import re
 
 for line in sys.stdin:
-    (key, page_rank, dest) = line.strip().split('\t')
-    print(line.strip())
+    line = line.strip()
+    (key, page_rank, dest) = line.split('\t')
+    print(line)
 
-    dest = re.sub(r'[{}]', '', dest)
-    dest = dest.split(',')
+    dest = dest[1:-1].split(',')
     for d in dest:
         print('%s\t%.3f\t{}' % (d, float(page_rank) / len(dest)))
